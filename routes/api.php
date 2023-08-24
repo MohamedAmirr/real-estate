d736e098a9c9939a85607bd7949bafc811043947
Mohamed Amir <mohamed.amir@robustastudio.com>
2023-08-24 08:24:00 +0000

    Admin login

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminSessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(['admin'],function (){
+    Route::post('admin/login',[AdminSessionController::class,'login']);
+    Route::post('admin/logout',[AdminSessionController::class,'logout'])
+        ->middleware(['auth:sanctum','ability:admin']);
 });
