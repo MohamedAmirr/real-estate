05f8f329f10efce6631d9ea1b5401dc61fd53528
Mohamed Amir <mohamed.amir@robustastudio.com>
2023-08-28 19:07:24 +0300

    reformat api file

--- routes/api.php
+++ routes/api.php
@@ -29,38 +29,40 @@ Route::prefix('admin')->group(function () {
     Route::post('login', [AdminSessionController::class, 'login']);
     Route::post('logout', [AdminSessionController::class, 'logout'])
         ->middleware(['auth:sanctum', 'ability:admin']);
+
+    Route::prefix('unit')->middleware(['auth:sanctum','ability:admin'])->group(function (){
+        Route::post('store',[UnitController::class,'store']);
+        Route::get('{unit}',[UnitController::class,'show']);
+        Route::delete('{unit}',[UnitController::class,'delete']);
+        Route::put('{unit}',[UnitController::class,'update']);
+    });
+
+    Route::middleware(['auth:sanctum','ability:admin'])->group(function (){
+        Route::get('users',[UserController::class,'show']);
+        Route::delete('user/{user}',[UserController::class,'destroy']);
+    });
 });
+
 
 Route::prefix('user')->group(function () {
     Route::post('register', [UserRegisterController::class, 'store']);
     Route::post('login', [UserSessionController::class, 'login']);
     Route::post('logout', [UserSessionController::class, 'logout'])
         ->middleware(['auth:sanctum', 'ability:user']);
-});
 
-Route::prefix('email')->group(function () {
-    Route::get('verify/{id}', [VerificationController::class, 'verify'])
-        ->name('verification.verify');
+    Route::prefix('email')->group(function () {
+        Route::get('verify/{id}', [VerificationController::class, 'verify'])
+            ->name('verification.verify');
 
-    Route::get('resend', [VerificationController::class, 'resend'])
-        ->name('verification.resend');
-});
-
-
-Route::prefix('unit')->group(function (){
-    Route::middleware(['auth:sanctum','ability:admin'])->group(function (){
-        Route::post('store',[UnitController::class,'store']);
-        Route::delete('{unit}',[UnitController::class,'delete']);
-        Route::put('{unit}',[UnitController::class,'update']);
+        Route::get('resend', [VerificationController::class, 'resend'])
+            ->name('verification.resend');
     });
 
-    Route::get('{unit}',[UnitController::class,'show'])
-        ->middleware(['auth:sanctum', 'ability:user,admin']);
-    Route::post('buy/{unit}',[UnitController::class,'buy'])
-        ->middleware(['auth:sanctum', 'ability:user']);;
+    Route::prefix('unit')->group(function (){
+        Route::get('{unit}',[UnitController::class,'show'])
+            ->middleware(['auth:sanctum', 'ability:user,admin']);
+        Route::post('buy/{unit}',[UnitController::class,'buy'])
+            ->middleware(['auth:sanctum', 'ability:user']);
+    });
 });
 
-Route::middleware(['auth:sanctum','ability:admin'])->group(function (){
-    Route::get('users',[UserController::class,'show']);
-    Route::delete('user/{user}',[UserController::class,'destroy']);
-});
